d0444df6af7135eea7ac1e030c449e00aff57ada
kaptast <tamas.kaptas@corvex.hu>
2019-10-01 16:31:51 +0200

    fix: Searchbar story with theme

--- ImageHUB/ClientApp/src/stories/3-SeachBar.stories.js
+++ ImageHUB/ClientApp/src/stories/3-SeachBar.stories.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import { action } from '@storybook/addon-actions';
 import SearchBar from '../components/SearchBar';
 import { BrowserRouter } from 'react-router-dom';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import red from '@material-ui/core/colors/red';
 
 export default {
     title: 'Searchbar',
 };
 
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#fff',
+        },
+        secondary: red,
+        error: red,
+        contrastThreshold: 3,
+        tonalOffset: 0.2,
+    },
+});
+
 export const searchbar = () => (
-    <BrowserRouter>
-        <SearchBar />
-    </BrowserRouter>
+    <MuiThemeProvider theme={theme}>
+        <BrowserRouter>
+            <SearchBar />
+        </BrowserRouter>
+    </MuiThemeProvider>
 );

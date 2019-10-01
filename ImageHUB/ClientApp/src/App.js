da0cb2587dfd2e553118383e3314387f1b6667f2
kaptast <tamas.kaptas@corvex.hu>
2019-10-01 16:20:42 +0200

    fix: added browserrouter

--- ImageHUB/ClientApp/src/App.js
+++ ImageHUB/ClientApp/src/App.js
@@ -1,16 +1,19 @@
 ﻿import React from 'react';
 import { Route, Switch } from 'react-router';
+import { BrowserRouter } from 'react-router-dom'
 import Layout from './components/Layout';
 import Home from './components/Home';
 import Profile from './components/Profile';
 import Messages from './components/Messages';
 
 export default () => (
-  <Layout>
-    <Switch>
-      <Route exact path='/' component={Home} />
-      <Route path='/profile' component={Profile} />
-      <Route path='/messages' component={Messages} />
-    </Switch>
-  </Layout>
+  <BrowserRouter>
+    <Layout>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/profile' component={Profile} />
+        <Route path='/messages' component={Messages} />
+      </Switch>
+    </Layout>
+  </BrowserRouter>
 );

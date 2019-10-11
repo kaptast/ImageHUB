03c1ff39d2722c1906acaf57b341bc5500cb9583
kaptast <kaptas.tamas@gmail.com>
2019-10-11 20:17:47 +0200

    Upload form

---
+++ ImageHUB/ClientApp/src/components/Upload/Upload.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { func } from 'prop-types';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import PropTypes from 'prop-types';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Dialog from '@material-ui/core/Dialog';
+import AddIcon from '@material-ui/icons/Add';
+import UploadForm from './UploadForm';
+
+function UploadDialog(props){
+    const { onClose, selectedValue, open } = props;
+
+    const handleClose = () => {
+        onClose();
+      };
+
+    return (
+        <Dialog onClose={handleClose} open={open}>
+            <DialogTitle>Upload an image</DialogTitle>
+            <UploadForm />
+        </Dialog>
+    );
+}
+
+export default function UploadButton(){
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = value => {
+    setOpen(false);
+    setSelectedValue(value);
+  };
+
+  return (
+    <div>
+      <IconButton color="inherit" onClick={handleClickOpen}>
+          <AddIcon />
+      </IconButton>
+      <UploadDialog open={open} onClose={handleClose} />
+    </div>
+  );
+}
---
+++ ImageHUB/ClientApp/src/components/Upload/UploadForm.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+import Button from '@material-ui/core/Button';
+import { DropzoneArea } from 'material-ui-dropzone';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import PublishIcon from '@material-ui/icons/Publish';
+
+const styles = theme => ({
+    container: {
+        display: 'flex',
+        justifyContent: 'flex',
+        alignItems: 'center',
+    },
+    button: {
+        margin: theme.spacing(1),
+      },
+});
+
+
+class UploadForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            file: []
+        };
+    }
+
+
+    onChange(files) {
+        this.setState({
+            file: files[0]
+        });
+    }
+
+    onFormSubmit(event) {
+        event.preventDefault();
+        const formData = new FormData();
+        formData.append("file", file);
+        axios.post("api/image/upload", formData, {
+            headers: { 'content-type': 'multipart/form-data' }
+        });
+    }
+
+    render() {
+        const { classes } = this.props;
+        return (
+            <div>
+                <form onSubmit={this.onFormSubmit}>
+                    <Grid container className={classes.container} spacing={2}>
+                        <Grid item xs={12}>
+                            <DropzoneArea acceptedFiles={['image/*']} filesLimit={1} name="img" onChange={this.onChange.bind(this)} />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Button
+                                type="submit"
+                                variant="contained"
+                                color="secondary"
+                                className={classes.button}
+                                startIcon={<PublishIcon />}
+                            >
+                                Upload
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </form>
+            </div>
+        )
+    }
+}
+
+UploadForm.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(UploadForm);

9b495ca8c10c6a638794ef5a94442e19aafd0b44
kaptast <tamas.kaptas@corvex.hu>
2019-10-15 16:38:17 +0200

    Fix loading

--- ImageHUB/ClientApp/src/store/Home.js
+++ ImageHUB/ClientApp/src/store/Home.js
@@ -1,42 +1,51 @@
+import axios from 'axios';
+
 const requestHomePostsType = 'REQUEST_HOME_POSTS';
 const receiveHomePostsType = 'RECEIVE_HOME_POSTS';
-const initialState = { homePosts: [], isLoading: false};
+const initialState = { homePosts: [], isLoading: false };
 
 export const actionCreators = {
-    requestHomePosts: index => async (dispatch, getState) => {
-        if (index === getState().homePosts.index) {
-            return;
-        }
-
-        dispatch({type: requestHomePostsType, index});
-
-        const url = `api/images`
-        const response = await fetch(url);
-        const homePosts = await response.json();
-
-        dispatch({type: receiveHomePostsType, index, homePosts});
+  requestHomePosts: index => async (dispatch, getState) => {
+    if (index === getState().homePosts.index) {
+      return;
     }
+
+    dispatch({ type: requestHomePostsType, index });
+
+    const url = `api/images`
+
+    axios.get(url)
+      .then(res => {
+        console.log("ok home store.");
+        console.log(res);
+        const homePosts = res.data;
+        dispatch({ type: receiveHomePostsType, index, homePosts });
+      }).catch(err => {
+        console.log(err);
+        console.log("to get images");
+      });
+  }
 };
 
 export const reducer = (state, action) => {
-    state = state || initialState;
-  
-    if (action.type === requestHomePostsType) {
-      return {
-        ...state,
-        index: action.index,
-        isLoading: true
-      };
-    }
-  
-    if (action.type === receiveHomePostsType) {
-      return {
-        ...state,
-        index: action.index,
-        homePosts: action.homePosts,
-        isLoading: false
-      };
-    }
-  
-    return state;
-  };
+  state = state || initialState;
+
+  if (action.type === requestHomePostsType) {
+    return {
+      ...state,
+      index: action.index,
+      isLoading: true
+    };
+  }
+
+  if (action.type === receiveHomePostsType) {
+    return {
+      ...state,
+      index: action.index,
+      homePosts: action.homePosts,
+      isLoading: false
+    };
+  }
+
+  return state;
+};
--- ImageHUB/ClientApp/src/store/Profile.js
+++ ImageHUB/ClientApp/src/store/Profile.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const requestProfileType = 'REQUEST_PROFILE';
 const receiveProfileType = 'RECEIVE_PROFILE';
 
@@ -6,45 +8,45 @@ const defaultProfile = {
   posts: [],
 }
 
-const initialState = { profile: defaultProfile, isLoading: false};
+const initialState = { profile: defaultProfile, isLoading: false };
 
 export const actionCreators = {
-    requestProfile: index => async (dispatch, getState) => {
-        if (index === getState().profile.index) {
-            return;
-        }
-        dispatch({type: requestProfileType, index});
-
-        const url = `api/profile`;
-        console.log(url);
-        const response = await fetch(url);
-        const profile = await response.json();
-
-        console.log(response);
-
-        dispatch({type: receiveProfileType, index, profile});
+  requestProfile: index => async (dispatch, getState) => {
+    if (index === getState().profile.index) {
+      return;
     }
+    dispatch({ type: requestProfileType, index });
+
+    const url = `api/profile`;
+    axios.get(url)
+      .then(res => {
+        console.log("ok home store.");
+        console.log(res);
+        const profile = res.data;
+        dispatch({ type: receiveProfileType, index, profile });
+      })
+  }
 };
 
 export const reducer = (state, action) => {
-    state = state || initialState;
-  
-    if (action.type === requestProfileType) {
-      return {
-        ...state,
-        index: action.index,
-        isLoading: true
-      };
-    }
-  
-    if (action.type === receiveProfileType) {
-      return {
-        ...state,
-        index: action.index,
-        profile: action.profile,
-        isLoading: false
-      };
-    }
-  
-    return state;
-  };
+  state = state || initialState;
+
+  if (action.type === requestProfileType) {
+    return {
+      ...state,
+      index: action.index,
+      isLoading: true
+    };
+  }
+
+  if (action.type === receiveProfileType) {
+    return {
+      ...state,
+      index: action.index,
+      profile: action.profile,
+      isLoading: false
+    };
+  }
+
+  return state;
+};

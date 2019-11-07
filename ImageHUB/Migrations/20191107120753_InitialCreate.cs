9e3f17b92abb9f6baa62a93f2a2798dbdb7374c8
kaptast <tamas.kaptas@corvex.hu>
2019-11-07 13:12:17 +0100

    Migrations

---
+++ ImageHUB/Migrations/20191107120753_InitialCreate.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ImageHUB.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Profiles",
+                columns: table => new
+                {
+                    ID = table.Column<string>(nullable: false),
+                    UserName = table.Column<string>(nullable: true),
+                    Avatar = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    ProfileID = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Profiles", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Profiles_Profiles_ProfileID",
+                        column: x => x.ProfileID,
+                        principalTable: "Profiles",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    UserName = table.Column<string>(nullable: true),
+                    Image = table.Column<string>(nullable: true),
+                    ID = table.Column<string>(nullable: false),
+                    ProfileID = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Posts_Profiles_ProfileID",
+                        column: x => x.ProfileID,
+                        principalTable: "Profiles",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_ProfileID",
+                table: "Posts",
+                column: "ProfileID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Profiles_ProfileID",
+                table: "Profiles",
+                column: "ProfileID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+
+            migrationBuilder.DropTable(
+                name: "Profiles");
+        }
+    }
+}

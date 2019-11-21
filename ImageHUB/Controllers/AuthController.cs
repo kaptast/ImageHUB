b2d34bd7deb58ca9f6f5bce9183edb0e5e283ddb
kaptast <kaptas.tamas@gmail.com>
2019-11-21 01:22:49 +0100

    Cleanup controllers

--- ImageHUB/Controllers/AuthController.cs
+++ ImageHUB/Controllers/AuthController.cs
@@ -1,35 +1,20 @@
-﻿using System.Security.Claims;
-using System.Threading.Tasks;
-using ImageHUB.Repositories;
-using ImageHUB.Services;
-using Microsoft.AspNetCore.Authentication;
+﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Facebook;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
-namespace ImageHUB.Controllers
+namespace imagehubsample.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class AuthController : ControllerBase
     {
-        private readonly IProfileService profileService;
-        public AuthController(IProfileService profileService)
-        {
-            this.profileService = profileService;
-        }
-
         [Route("isloggedin")]
         [Authorize]
         public string IsLoggedIn()
         {
-            /*var id = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            var username = User.FindFirstValue(ClaimTypes.Name);
-
-            var user = this.profileService.GetProfileByID(id, username);
-
-            return user;*/
             return HttpContext.User.Identity.Name;
         }
 

59add8dd5dd443c6dcae348c1e3fb792da0e3ca6
kaptast <kaptas.tamas@gmail.com>
2019-11-26 20:51:15 +0100

    Add controllers

---
+++ ImageHUB/Controllers/FriendController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using ImageHUB.Repositories;
+using ImageHUB.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImageHUB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FriendController : ControllerBase
+    {
+        private readonly IProfileService profileService;
+
+        public FriendController(IProfileService profileService)
+        {
+            this.profileService = profileService;
+        }
+
+        [HttpPost]
+        [Route("AddFriend")]
+        [Authorize]
+        public void AddFriend(string id)
+        {
+            string userName = HttpContext.User.Identity.Name;
+            var userId = HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            System.Diagnostics.Trace.WriteLine(string.Format("AddFriend userID: {0}", userId));
+
+            this.profileService.AddFriend(userId, id);
+        }
+
+        [HttpPost]
+        [Route("AcceptFriend")]
+        [Authorize]
+        public void AcceptFriend(string id)
+        {
+            string userName = HttpContext.User.Identity.Name;
+            var userId = HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            this.profileService.AcceptFriend(userId, id);
+        }
+    }
+}

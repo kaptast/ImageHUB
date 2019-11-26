e6d74545aa892cfb64f8ee46d78058927278165f
kaptast <kaptas.tamas@gmail.com>
2019-11-26 19:33:46 +0100

    fix build error

--- ImageHUB/Services/ProfileService.cs
+++ ImageHUB/Services/ProfileService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ImageHUB.Repositories;
+using imagehubsample;
 using Microsoft.Extensions.Logging;
 
 namespace ImageHUB.Services

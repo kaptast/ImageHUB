508df01fb871f96c0ac4b0272661217f6505d537
kaptast <kaptas.tamas@gmail.com>
2019-11-26 19:21:57 +0100

    Add Repository

---
+++ ImageHUB/Repositories/DatabaseContext.cs
@@ -0,0 +1,136 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ImageHUB.Repositories
+{
+    public class DatabaseContext : DbContext, IDatabaseContext
+    {
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Profile> Profiles { get; set; }
+
+        private object lockObject = new object();
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options)
+        {
+            Database.Migrate();
+        }
+
+        /*public DatabaseContext()
+        {
+            Database.Migrate();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
+            optionsBuilder.UseMySql(Environment.GetEnvironmentVariable("MYSQLCONNSTR_localdb").ToString());
+        }*/
+
+        public IEnumerable<Post> GetAllPosts(string userID)
+        {
+            var friends = this.GetFriends(userID, false);
+            return this.Posts?.Include(p => p.Owner).Where(p => p.Owner.UserID.Equals(userID) || friends.Contains(p.Owner))?.OrderByDescending(p => p.ID)?.ToList();
+        }
+
+        public IEnumerable<Post> GetPostByUserID(string id)
+        {
+            return this.Posts?.Include(p => p.Owner).Where(p => p.Owner.UserID.Equals(id))?.OrderByDescending(p => p.ID).ToList();
+        }
+
+        public void SaveImage(string path, Profile owner)
+        {
+            using (var transaction = this.Database.BeginTransaction())
+            {
+                this.Posts.Add(new Post()
+                {
+                    Image = path,
+                    Owner = owner
+                });
+
+                this.SaveChanges();
+                transaction.Commit();
+            }
+        }
+
+        public IEnumerable<Profile> GetProfiles()
+        {
+            return this.Profiles?.ToList();
+        }
+
+        public Profile GetProfileByID(string id)
+        {
+            lock (lockObject)
+            {
+                return this.Profiles.Where(p => p.UserID.Equals(id)).Include(ft => ft.FriendsTo).ThenInclude(ft => ft.Friend).Include(fw => fw.FriendsWith).ThenInclude(fw => fw.Profile).SingleOrDefault();
+            }
+        }
+
+        public void AddNewProfile(Profile profile)
+        {
+            using (var transaction = this.Database.BeginTransaction())
+            {
+                this.Profiles?.Add(profile);
+                this.SaveChanges();
+                transaction.Commit();
+            }
+        }
+
+        public IEnumerable<Profile> GetProfilesByName(string name)
+        {
+            return this.Profiles?.Where(x => x.UserName.ToLower().Contains(name.ToLower())).ToList();
+        }
+
+        public IEnumerable<Profile> GetFriends(string userID, bool selectPending = false)
+        {
+            var user = this.GetProfileByID(userID);
+            if (user != null)
+            {
+                var friendsTo = user.FriendsTo?.Where(p => p.Accepted || selectPending).Select(x => x.Friend)?.ToList();
+                var friendsWith = user.FriendsWith?.Where(p => p.Accepted || selectPending).Select(x => x.Profile)?.ToList();
+                var friends = friendsTo?.Concat(friendsWith);
+                return friends;
+            }
+
+            return new List<Profile>();
+        }
+
+        public ProfileFriend GetFriendShip(string userID, string friendID)
+        {
+            var user = this.GetProfileByID(userID);
+            if (user == null) return null;
+
+            return user.FriendsTo?.Where(pf => pf.ProfileID.Equals(userID) && pf.FriendID.Equals(friendID)).SingleOrDefault();
+        }
+
+        public void AddFriend(string userID, string friendID)
+        {
+            using (var transaction = this.Database.BeginTransaction())
+            {
+                var friendProfile = this.GetProfileByID(friendID);
+                var user = this.GetProfileByID(userID);
+                var p2f = new ProfileFriend();
+                p2f.Profile = user;
+                p2f.Friend = friendProfile;
+                user.FriendsTo.Add(p2f);
+
+                this.SaveChanges();
+                transaction.Commit();
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProfileFriend>().HasKey(bc => new { bc.ProfileID, bc.FriendID });
+            modelBuilder.Entity<ProfileFriend>().HasOne(bc => bc.Profile).WithMany(b => b.FriendsTo).HasForeignKey(bc => bc.ProfileID);
+            modelBuilder.Entity<ProfileFriend>().HasOne(bc => bc.Friend).WithMany(c => c.FriendsWith).HasForeignKey(bc => bc.FriendID);
+        }
+
+        public void UpdateFriendShip(ProfileFriend friendship)
+        {
+            this.Update(friendship);
+            this.SaveChanges();
+        }
+    }
+}
---
+++ ImageHUB/Repositories/Post.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ImageHUB.Repositories
+{
+    public class Post
+    {
+        public string Image { get; set; }
+        public int ID { get; set; }
+
+        public bool show { get { return true; } }
+
+        [JsonIgnore]
+        public Profile Owner { get; set; }
+
+        [NotMapped]
+        public ProfileDTO OwnerDTO
+        {
+            get
+            {
+                return new ProfileDTO()
+                {
+                    ID = this.Owner.UserID,
+                    UserName = this.Owner.UserName,
+                    Avatar = this.Owner.UserID,
+                };
+            }
+            set { }
+        }
+    }
+
+    public class ProfileDTO
+    {
+        public string ID { get; set; }
+        public string UserName { get; set; }
+
+        public string Avatar { get; set; }
+
+        public string Email { get; set; }
+    }
+}

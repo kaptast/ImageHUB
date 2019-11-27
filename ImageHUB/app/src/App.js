cef50119f13c1f399cf8250e205ae1ded0a07a49
kaptast <tamas.kaptas@corvex.hu>
2019-11-27 14:45:05 +0100

    Remove unused css

--- ImageHUB/app/src/App.js
+++ ImageHUB/app/src/App.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-import './custom.css'
-
 export default function App() {
     
     const [isLoggedIn, setIsLoggedIn] = useState(false)

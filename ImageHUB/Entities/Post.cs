4b29f51fa7320c7f1a0981cfddacd1a1c93793d9
kaptast <kaptas.tamas@gmail.com>
2019-11-29 01:25:31 +0100

    Entities

---
+++ ImageHUB/Entities/Post.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ImageHUB.Entities
+{
+    public class Post : IEntity
+    {
+        public string Image { get; set; }
+        public int ID { get; set; }
+        public bool show { get { return true; } }
+
+        [JsonIgnore]
+        public Profile Owner { get; set; }
+
+        [NotMapped]
+        public ProfileDTO OwnerDTO
+        {
+            get
+            {
+                return new ProfileDTO(this.Owner);
+            }
+        }
+
+    }
+}
---
+++ ImageHUB/Entities/Profile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageHUB.Entities
+{
+    public class Profile : IEntity
+    {
+        public int ID { get; set; }
+        public string UserID { get; set; }
+        public string UserName { get; set; }
+    }
+
+    public class ProfileDTO
+    {
+        public string ID { get; set; }
+        public string UserName { get; set; }
+
+        public ProfileDTO(Profile profile)
+        {
+            this.ID = profile.UserID;
+            this.UserName = profile.UserName;
+        }
+    }
+}

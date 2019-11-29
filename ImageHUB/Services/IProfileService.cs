ee6c45914dc0f88524ce551dd8ee76224caaf0ab
kaptast <kaptas.tamas@gmail.com>
2019-11-29 01:26:06 +0100

    Services

---
+++ ImageHUB/Services/IProfileService.cs
@@ -0,0 +1,15 @@
+﻿using ImageHUB.Entities;
+using ImageHUB.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageHUB.Services
+{
+    public interface IProfileService
+    {
+        Profile GetProfileByID(string userID, string userName);
+        IEnumerable<Profile> GetProfilesByName(string userName);
+    }
+}
---
+++ ImageHUB/Services/ProfileService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ImageHUB.Entities;
+using ImageHUB.Repositories;
+
+namespace ImageHUB.Services
+{
+    public class ProfileService : IProfileService
+    {
+        private readonly IProfileRepository repository;
+
+        public ProfileService(IProfileRepository repo)
+        {
+            this.repository = repo;
+        }
+        private Profile AddProfile(string userID, string userName)
+        {
+            var profile = new Profile()
+            {
+                UserID = userID,
+                UserName = userName
+            };
+
+            this.repository.Add(profile);
+
+            return profile;
+        }
+
+        public Profile GetProfileByID(string userID, string userName)
+        {
+            var profile = this.repository.GetByID(userID);
+
+            if (profile == null)
+            {
+                profile = AddProfile(userID, userName);
+            }
+
+            return profile;
+        }
+
+        public IEnumerable<Profile> GetProfilesByName(string userName)
+        {
+            var profiles = this.repository.GetProfilesByName(userName);
+
+            return profiles;
+        }
+    }
+}

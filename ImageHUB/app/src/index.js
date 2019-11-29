edd0aa6569145579a3b6a6867b88f8b5271efe0f
kaptast <kaptas.tamas@gmail.com>
2019-11-29 20:54:28 +0100

    Material ui

--- ImageHUB/app/src/index.js
+++ ImageHUB/app/src/index.js
@@ -1,15 +1,45 @@
-import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import red from '@material-ui/core/colors/red';
 import App from './App';
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#fff',
+    },
+    secondary: red,
+    error: red,
+    contrastThreshold: 3,
+    tonalOffset: 0.2,
+  },
+  typography: {
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      'Roboto',
+      '"Segoe UI"',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
+});
+
 ReactDOM.render(
-  <BrowserRouter basename={baseUrl}>
-    <App />
-  </BrowserRouter>,
+  <MuiThemeProvider theme={theme}>
+    <BrowserRouter basename={baseUrl}>
+      <App />
+    </BrowserRouter>
+  </MuiThemeProvider>,
   rootElement);
 
